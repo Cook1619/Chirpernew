dfbd496ee8fa0c13e4f5c51b53b4927a96565596
Matt <cook6348@gmail.com>
2018-12-13 06:50:43 -0600

    changed color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ class Header extends Component {
         return (
            <nav className="Nav">
              <div className="Nav-menus">
-               <div className="Nav-brand display-3 d-flex justify-content-center">
+               <div className="Nav-brand display-3 d-flex justify-content-center p-5">
                  <h1>Chirper</h1>
                </div>
              </div>
--- src/components/NewCard.jsx
+++ src/components/NewCard.jsx
@@ -4,7 +4,7 @@ const Newcard = (props) => {
 
     return (
         <div className="card bg-light mb-3 m-0 ">
-            <div className="card-header bg-primary text-white">{props.cardDetails.username}</div>
+            <div className="card-header bg-dark text-white">{props.cardDetails.username}</div>
             <div className="card-body ">
                 <h5 className="card-title">{props.cardDetails.chirp}</h5>
             </div>
