ec06f32040429b7952d4f7328c1e3e29138cf42d
Matt <cook6348@gmail.com>
2018-12-13 07:08:01 -0600

    Updates

---
+++ precache-manifest.106959ac199d0556c0f9b2d702759249.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38c1dc6515f2c3a2c3ba",
+    "url": "/Chirpernew/static/js/runtime~main.38c1dc65.js"
+  },
+  {
+    "revision": "42e900f81d0f2538d1ac",
+    "url": "/Chirpernew/static/js/main.42e900f8.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/Chirpernew/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "42e900f81d0f2538d1ac",
+    "url": "/Chirpernew/static/css/main.4280a060.chunk.css"
+  },
+  {
+    "revision": "51215d1db533bc1d8158631883c1169b",
+    "url": "/Chirpernew/index.html"
+  }
+];
